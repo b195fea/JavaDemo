175d80778e1de392f5dfe4e1e6c1cf2c81a0801e
terence.chung <b195fea@gmail.com>
2019-08-02 07:34:03 +0800

    commit

--- JavaDemo/src/main/java/idv/zjh/thread/pool/CachedThreadPool.java
+++ JavaDemo/src/main/java/idv/zjh/thread/pool/CachedThreadPool.java
@@ -3,6 +3,10 @@ package idv.zjh.thread.pool;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * 創建一個具有可緩存功能的線程池
+ * @author zjh
+ */
 public class CachedThreadPool {
 
 	public static void main(String[] args) {
