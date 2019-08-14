e799892eb08b2096062ec11136c0fc1a518fd006
terence.chung <b195fea@gmail.com>
2019-08-14 22:24:52 +0800

    1.增加 Thread 跟 Runnable 簡單實作

--- JavaDemo/src/main/java/idv/zjh/Test.java
+++ JavaDemo/src/main/java/idv/zjh/Test.java
@@ -1,8 +1,18 @@
 package idv.zjh;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+//		Map map = new HashMap<String, String>();
+//		map.put(key, value);
+		
+		System.out.println(13 & 3);
+		//01101
+		//00001
+		
 	}
 }
